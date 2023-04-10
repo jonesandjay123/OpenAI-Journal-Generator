4d94a7ccb3fabcd4b4f8d6321c98399dd2da1213
Jones Wang <jonesandjay123@yahoo.com.tw>
2023-04-10 17:00:11 -0400

    add utf-8 declare

--- 3.train_data_split.py
+++ 3.train_data_split.py
@@ -1,12 +1,12 @@
 from sklearn.model_selection import train_test_split
 
-with open("train_data.txt", "r") as f:
+with open("train_data.txt", "r", encoding="utf-8") as f:
     data = f.readlines()
 
 train_data, val_data = train_test_split(data, test_size=0.1)
 
-with open("train.txt", "w") as f:
+with open("train.txt", "w", encoding="utf-8") as f:
     f.writelines(train_data)
 
-with open("val.txt", "w") as f:
+with open("val.txt", "w", encoding="utf-8") as f:
     f.writelines(val_data)
