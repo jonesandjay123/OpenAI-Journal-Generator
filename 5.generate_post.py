9fa11b664598f005a1c170ba2bd68490c983e339
Jones Wang <jonesandjay123@yahoo.com.tw>
2023-04-10 22:55:09 -0400

    udpate generate_post

--- 5.generate_post.py
+++ 5.generate_post.py
@@ -1,11 +1,24 @@
 from transformers import pipeline
 
-model_path = "output"
+model_path = "output/checkpoint-800"
+model_name = "gpt2"
 generator = pipeline("text-generation", model=model_path, tokenizer=model_name, device=0)
 
 def generate_text(prompt, max_length=150):
     return generator(prompt, max_length=max_length, num_return_sequences=1)[0]["generated_text"]
 
-prompt = "今天天氣很好"  # 您想要提供的主題
-output_text = generate_text(prompt)
-print(output_text)
+prompts = [
+    "今天天氣很好",
+    "和冠傑他們去爬山",
+    "認識了叫做EVA的新朋友",
+    "吃了好吃的餐廳名叫皇椰雞"
+]
+
+generated_article = ""
+
+for prompt in prompts:
+    output_text = generate_text(prompt)
+    generated_article += output_text + " "
+
+print("Generated article:")
+print(generated_article)
